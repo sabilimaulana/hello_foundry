9b55421b1a41b6c0a2197fbb82825539106d960f
sabilimaulana <spacysbl.learn@gmail.com>
2022-10-24 16:36:07 +0700

    chore: using safe math

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "openzeppelin-contracts/utils/math/SafeMath.sol";
+
 contract Counter {
+    using SafeMath for uint256;
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
@@ -9,6 +13,6 @@ contract Counter {
     }
 
     function increment() public {
-        number++;
+        number = number.add(1);
     }
 }
